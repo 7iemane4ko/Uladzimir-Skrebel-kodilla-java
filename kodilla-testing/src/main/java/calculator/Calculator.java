26a67c6c6250650f717a755e55e0cf892b49e853
Uladzimir Skrebel <vovka.skrebel@gmail.com>
2023-06-12 16:51:03 +0200

    6.1 Added kodilla-testing module

---
+++ kodilla-testing/src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+    public double addAToB(double a, double b) {
+        return a + b;
+    }
+
+    public double substractAFroB (double a, double b) {
+        return a - b;
+    }
+}
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,44 @@
 package com.kodilla.testing;
 
+import calculator.Calculator;
+import user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        // test pierwszy->
+        System.out.println("Test - pierwszy SimpleUser test jednostkowy:");
+        //
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // kolejny test ->
+        System.out.println("Test - drugi Calculator (pierwsza metoda addAToB) test jednostkowy:");
+        //
+
+        Calculator Calculator = new Calculator();
+
+        double resultOne = Calculator.addAToB(8.4, 8.4);
+
+        System.out.println(result);
+
+        // kolejny test ->
+        System.out.println("Test - drugi Calculator (druga metoda addAToB) test jednostkowy:");
+        //
+
+        Calculator simpleCalculator = new Calculator();
+
+        double resultTwo = simpleCalculator.substractAFroB(8.4, 8.4);
+
+        System.out.println(result);
+
     }
 }
